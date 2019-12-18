737a1070d9dab68ade41a203befd491c1c7cde97
Daniel Reuter <dreuter@kth.se>
2019-12-18 03:44:50 +0100

    update

--- encoding/05_extract_unformatted_lists.py
+++ encoding/05_extract_unformatted_lists.py
@@ -1,10 +1,12 @@
 import os, re
+from itertools import combinations
 import pandas as pd
 
 DATA_DIR = 'data'
 ENCODED_DOCUMENTS_DIR = 'diffrent_encoded_text_data'
 ENCODED_DOCUMENTS_PATH = os.path.join('..',DATA_DIR, ENCODED_DOCUMENTS_DIR)
 encoded_files = os.listdir(ENCODED_DOCUMENTS_PATH)
+print(encoded_files)
 encodings = ['iso8859_15','latin_1','cp1252','cp1252','utf_8','iso8859_3','iso8859_10']
 
 def checklength(object, n):
@@ -25,7 +27,7 @@ def clean_qalistfile(filename, encoding):
             if not line[-1:] == '.' and len(line) == 2:
                 line+='.' # Add missing last punctuation
             line+=';' # and then a semicolon marker
-
+            
             regsplit = re.findall(r".*\?|\S.*?\.|\S.*?\;|\ .*?\;", line) # Split by pattern
             if len(regsplit) == 2:
                 regsplit[1] = regsplit[1][:-1] # Remove semicolon marker
@@ -50,9 +52,19 @@ def main():
         filename = os.path.join(ENCODED_DOCUMENTS_PATH,filename)
         qadf = clean_qalistfile(filename, encoding)
         dfs.append(qadf)
-        print('')
-        print(encoding)
-        print(qadf.tail(50))
+    
+    n_dfs = len(dfs)
+    print(n_dfs)
+    
+    for df1, df2 in combinations(range(n_dfs), 2):
+        check_equal = (dfs[df1] == dfs[df2])
+        check_false = False in check_equal.values
+        if not check_false:
+            print('Dataframe',df1,'and',df2,'has no element that differ, corresponds to:')
+            print(encoded_files[df1],'and',encoded_files[df2],'\n')
+        else:
+            #print('Dataframe', df1, 'and', df2, 'has at least one element that differ')
+            pass
 
 if __name__=='__main__':
     main()
