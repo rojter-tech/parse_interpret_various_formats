222ea1638bffe4c15d8877e710fc67e78b9d4bb8
Daniel Reuter <dreuter@kth.se>
2019-12-18 07:29:56 +0100

    update

--- 00_xml_minidom_word.py
+++ 00_xml_minidom_word.py
@@ -1,13 +1,14 @@
 import os, zipfile, re
 import xml.dom.minidom as minidom
+
+DATADIR = 'data'
+FILENAME = 'Word_Questions2'
+EXTENSION = '.docx'
+FILEPATH = os.path.join(DATADIR, FILENAME + EXTENSION)
+
 def main():
     print("Word Document Parser")
-    DATADIR = 'data'
-    FILENAME = 'Word_Questions2'
-    EXTENSION = '.docx'
-    FILEPATH = os.path.join(DATADIR, FILENAME + EXTENSION)
     docx_file = zipfile.ZipFile(FILEPATH)
-
     documentname = ""
     for name in docx_file.namelist():
         if re.match(r".*document.*", str(name)):
