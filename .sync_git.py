69640acc4f971d4b6158d73ba9fc324f60c8cdfc
Daniel Reuter <dreuter@kth.se>
2019-12-26 04:36:30 +0100

    test windows

--- .sync_git.py
+++ .sync_git.py
@@ -19,9 +19,11 @@ print(os.path.isdir(GITHUBPATH))
 PROJECTPATH = os.path.join(HOMEPATH,
                            "Environments",
                            "parse_interpret_various_formats")
-LOGS_DIR = "logs"
+
+LOGS_DIR = ".logs"
 print("Using Github path:", GITHUBPATH)
 print("Using Project path:", PROJECTPATH)
+
 
 def rsync_local(source, target, reference):
     logs_dir = LOGS_DIR
@@ -55,18 +57,20 @@ def main(arguments):
         elif argument == "--sync":
             sync = True
 
+
     def gitpush():
         logpath = os.path.join(PROJECTPATH,LOGS_DIR,'gitpush.log')
         with open(logpath, 'wb') as f: pass
         msg = input("Commit message: ")
-        msg = "'" + msg + "'"
+        msg = '"' + msg + '"'
         cmd_git = "git add ."
         cmd_request(cmd_git, logpath, basedir=PROJECTPATH)
         cmd_git = "git commit -m " + msg
         cmd_request(cmd_git, logpath, basedir=PROJECTPATH)
         cmd_git = "git push"
         cmd_request(cmd_git, logpath, basedir=PROJECTPATH)
-    
+
+
     def gitpull():
         logpath = os.path.join(PROJECTPATH,LOGS_DIR,'gitpull.log')
         with open(logpath, 'wb') as f: pass
@@ -74,37 +78,43 @@ def main(arguments):
         cmd_request(cmd_git, logpath, basedir=GITHUBPATH)
         cmd_git = "git pull"
         cmd_request(cmd_git, logpath, basedir=GITHUBPATH)
-    
+
+
     def od_upload():
         logpath = os.path.join(HOMEPATH, LOGS_DIR, "odsync.log")
         cmd_request('onedrive --synchronize --upload-only', logpath)
-    
+
+
     def od_download():
         logpath = os.path.join(HOMEPATH, LOGS_DIR, "odsync.log")
         cmd_request('onedrive --synchronize --download-only', logpath)
-    
+
+
     def od_sync():
         logpath = os.path.join(HOMEPATH, LOGS_DIR, "odsync.log")
         cmd_request('onedrive --synchronize', logpath)
-    
+
+
     if git:
         gitpush()
         gitpull()
+
     if onedrive and not git:
         od_download()
         rsync_local(GITHUBPATH, PROJECTPATH, GITHUBPATH)
+
     elif onedrive and git:
         rsync_local(PROJECTPATH, GITHUBPATH, GITHUBPATH)
         rsync_local(GITHUBPATH, PROJECTPATH, GITHUBPATH)
         od_upload()
+
     if sync and not git and not onedrive:
         od_sync()
-    
+
 
 if __name__ == "__main__":
     try:
         arguments = sys.argv[1:]
-        arguments = ['--git']
         main(arguments)
     except IndexError:
         print("No argument given!")
